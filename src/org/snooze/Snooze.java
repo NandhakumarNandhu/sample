99bc2755a141fba3014658b8261b9e8025360b16
NandhakumarNandhu <nandhakumar8224@gmail.com>
2022-02-22 20:44:53 +0530

    done

---
+++ src/org/snooze/Snooze.java
@@ -0,0 +1,26 @@
+package org.snooze;
+
+public class Snooze {
+
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("nandhu");
+		
+		System.out.println("kumar");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
